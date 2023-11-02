7793126e0314154d1646171c3de5e613220e785e
alyafaraht <128797081+alyafaraht@users.noreply.github.com>
2023-11-02 16:48:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 21:59:27 2023
+
+@author: alyafaraht
+"""
+
+import ael as L
+
+image_folder_L = "/Users/alyafaraht/Downloads/ael/assets/"
+
+ael_result = L.call_ael(image_folder_L)
+ael_result
+print (ael_result) #--- just to check
+
